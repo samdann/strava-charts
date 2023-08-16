606027516941f075ef1c1c9427e5bf8ec4a01491
samdann <sami.dannoune@gmail.com>
2023-08-16 16:12:56 +0200

    get run activity with max heart rate

--- src/main/java/com/strava/charts/controller/StatisticsController.java
+++ src/main/java/com/strava/charts/controller/StatisticsController.java
@@ -1,13 +1,13 @@
 package com.strava.charts.controller;
 
-import com.strava.charts.service.AuthorizationService;
+import com.strava.charts.model.primary.Activity;
 import com.strava.charts.service.StatisticsService;
 import io.micrometer.core.annotation.Timed;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import io.swagger.client.ApiException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,19 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatisticsController {
 
      @Autowired
-     AuthorizationService authorizationService;
-
-     @Autowired
      StatisticsService statisticsService;
 
      @ApiOperation(value = "getMaxHeartRate", tags = "statistics-api")
      @RequestMapping(value = "/_get-max-heart-rate", produces = {
              "application/json"}, method = RequestMethod.GET)
-     public int getMaxHeartRate(
-             @ApiParam(name = "code", value = "authorization code") @RequestParam(value = "code") String code)
-             throws ApiException {
+     public ResponseEntity<Activity> getMaxHeartRate(
+             @ApiParam(name = "code", value = "authorization code") @RequestParam(value = "code") String code) {
 
-          return statisticsService.getMaxHearRateByActivityType();
+          return ResponseEntity.ok()
+                  .body(statisticsService.getMaxHearRateByActivityType());
 
      }
 
--- src/main/java/com/strava/charts/service/StatisticsService.java
+++ src/main/java/com/strava/charts/service/StatisticsService.java
@@ -3,7 +3,7 @@ package com.strava.charts.service;
 import com.strava.charts.model.primary.Activity;
 import com.strava.charts.repository.ActivityRepository;
 import io.swagger.client.model.ActivityType;
-import java.util.Optional;
+import java.util.Comparator;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,17 +16,14 @@ public class StatisticsService {
      ActivityRepository activityRepository;
 
      /**
-      * @param activitiesApi native API
-      * @param createdAt     date of creation
       * @return the max heart rate number for the given input
       */
-     public int getMaxHearRateByActivityType() {
+     public Activity getMaxHearRateByActivityType() {
 
-          Optional<Integer> max = activityRepository.findAll().stream()
+          return activityRepository.findAll().stream()
                   .filter(activity -> ActivityType.RUN.equals(activity.getType()))
-                  .map(Activity::getMaxHeartRate).max(Integer::compareTo);
+                  .max(Comparator.comparingInt(Activity::getMaxHeartRate)).orElse(null);
 
-          return max.orElse(0);
      }
 
 }
